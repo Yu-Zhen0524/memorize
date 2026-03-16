cbef474b69f94b36f73f6805ab1a86261b8877b3
Yu-Zhen0524 <brenda55305208@email.com>
2026-03-16 16:31:03 +0800

    Initial Commit

---
+++ memorise/memoriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  memoriseApp.swift
+//  memorise
+//
+//  Created by 11147221 on 2026/3/16.
+//
+
+import SwiftUI
+
+@main
+struct memoriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
